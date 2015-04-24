b62ac2d4d91e5d831fe3fe016d845011d4d72606
itomblack <itomblack@Ians-MacBook-Pro.local>
2015-04-24 12:58:43 +0100

    draw objects on page load

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,51 @@
 $( document ).ready(function() {
 
 
-  $( "#insert-svg" ).load( "pebble-svg-2.html");
+  $( "#insert-svg" ).load("pebble-svg.html", function() {
+
+     var current_frame = 0;
+    var total_frames = 60;
+    var path = [];
+    var length = [];
+
+    path = document.querySelectorAll('path');
+
+    for(var i=0; i<path.length;i++){
+
+      l = path[i].getTotalLength();
+      length[i] = l;
+      path[i].style.strokeDasharray = l + ' ' + l; 
+      path[i].style.strokeDashoffset = l;
+    }
+    var handle = 0;
+
+    var draw = function() {
+       var progress = current_frame/total_frames;
+       if (progress > 1) {
+         window.cancelAnimationFrame(handle);
+       } else {
+         current_frame++;
+         for(var j=0; j<path.length;j++){
+           path[j].style.strokeDashoffset = Math.floor(length[j] * (1 - progress));
+         }
+         handle = window.requestAnimationFrame(draw);
+       }
+    };
+    draw();
+
+
+
+  });
+
+
+
+
+
+  // $('#run').click(function() {
+
+   
+
+  // });
 
 
 
